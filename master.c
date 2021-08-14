e9b19ebb767b2fb646ea91ee2edb47afd2714939
AdamJaneczech <anndyjan@seznam.cz>
2021-08-15 00:30:41 +0200

    overflow-working-incorrect-data

--- master.c
+++ master.c
@@ -1,34 +1,34 @@
 #define F_CPU 8000000
 
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
 char data = 169;
+
+ISR(USI_OVF_vect){
+    cli();
+    PORTB ^= 1 << PB3;
+    //USISR |= 1<<USIOIF; //clean the counter overflow flag
+    sei();
+}
 
 void SPI_write(){
     //PORTB &= ~(1 << PB3);   //SS pin LOW
     USIDR = data;    //load the actual character into the USI data register
-    if(USIOIF == 0b0){   
-        PORTB ^= 1<<PB3;
-        _delay_ms(500);
-        PORTB ^= 1<<PB3;
-        _delay_ms(50);
-    }
-    while(USIOIF==0){
+    while(USISR >= 0){ //counter counts from 15 to 0
+        //USICR |= (1<<USIWM0)|(1<<USITC);    //toggle the clock pin
+        _delay_ms(10);
         USICR |= (1<<USIWM0)|(1<<USICLK)|(1<<USITC);    //toggle the clock pin
+        _delay_ms(10);
     }
-    /*for(char y = 0; y < 8; y++){
-        USICR = (1<<USIWM0)|(1<<USITC);    //toggle the clock pin
-        USICR = (1<<USIWM0)|(1<<USICLK)|(1<<USITC);  //toggle the register shifter & the clock pin
-    }*/
-    /*if(USISR & 1<<USIOIF){
-        PORTB |= 1 << PB3;
-    }*/
-    USISR |= 1<<USIOIF; //clean the counter overflow flag
+    //USISR |= 1<<USIOIF; //clean the counter overflow flag
 }
 
 void SPI_setup(){
+    USICR |= 1 << USIOIE;   //Enable counter overflow interrupt
     USICR |= 1 << USIWM0;   //Three-wire mode (SPI)
+    sei();
 }
 
 int main(void){
@@ -45,7 +45,6 @@ int main(void){
     while(1){
         if(PINB & (1<<PB4)){
             SPI_write();
-        }
-            
+        } 
     }
 }
