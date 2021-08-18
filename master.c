d42ccaef2743ac856417752e40b2397d03ed9669
AdamJaneczech <anndyjan@seznam.cz>
2021-08-18 15:34:26 +0200

    first-working

--- master.c
+++ master.c
@@ -4,7 +4,7 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-char data = 169;
+uint8_t data = 169;
 
 ISR(USI_OVF_vect){
     cli();
@@ -15,28 +15,31 @@ ISR(USI_OVF_vect){
 
 void SPI_write(){
     //PORTB &= ~(1 << PB3);   //SS pin LOW
+    USISR |= 1<<USIOIF;
     USIDR = data;    //load the actual character into the USI data register
-    while(USISR >= 0){ //counter counts from 15 to 0
-        //USICR |= (1<<USIWM0)|(1<<USITC);    //toggle the clock pin
-        _delay_ms(10);
-        USICR |= (1<<USIWM0)|(1<<USICLK)|(1<<USITC);    //toggle the clock pin
-        _delay_ms(10);
+    while(!(USISR & 1<<USIOIF)){ //counter counts from 15 to 0
+        _delay_ms(250);
+        USICR |= (1<<USIWM0)|(1<<USICS1)|(1<<USICLK)|(1<<USITC);
+        _delay_ms(250);
     }
+    USICR |= (1<<USIWM0)|(1<<USITC);
+    USISR |= 1<<USIOIF;
     //USISR |= 1<<USIOIF; //clean the counter overflow flag
 }
 
 void SPI_setup(){
-    USICR |= 1 << USIOIE;   //Enable counter overflow interrupt
-    USICR |= 1 << USIWM0;   //Three-wire mode (SPI)
+    //USICR |= 1 << USIOIE;   //Enable counter overflow interrupt
+    USICR |= 1 << USIWM0 |/* 1 << USICS1 | */1 << USICLK;   //Three-wire mode (SPI)
     sei();
 }
 
 int main(void){
-    DDRB |= 1 << PB3;   //LED indicator
-    DDRB |= 1 << PB2;   //SCK output
-    //PB1 set as input (default)
-    DDRB |= 1 << PB0;   //MOSI
+    DDRB |= 1 << DDB3;   //LED indicator
+    DDRB |= 1 << DDB2;   //SCK output
+    DDRB |= 1 << DDB1;   //DO
+    //PB0 set as input (default) -> DI
     //LED start-up blink
+    PORTB &= ~(1 << PB0);
     PORTB ^= 1 << PB3;
     _delay_ms(200);
     PORTB ^= 1 << PB3;
